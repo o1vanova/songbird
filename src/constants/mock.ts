ab3173a1935840721bccb487b55b288d689c7881
Olga Ivanova <olgai1409@gmail.com>
2020-02-12 22:26:34 +0500

    feat: add birds which live in Africa

--- src/constants/mock.ts
+++ src/constants/mock.ts
@@ -1,54 +1,54 @@
-const categories = [ 'first', 'second', 'third', 'fourth', 'fifth', 'sixth' ];
+const categories = [ 'Africa', 'Europe', 'America', 'Oceania', 'Asia', 'Australia' ];
 
 const birdsData = [
   [
     {
       id: 1,
-      name: 'Ворон',
-      species: 'Corvus corax',
-      description: 'Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.',
-      image: 'https://live.staticflickr.com//65535//49298804222_474cfe8682.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3'
+      name: 'Клушица',
+      species: 'Pyrrhocorax pyrrhocorax',
+      description: 'Клуши́ца, красноклювая альпийская ворона — птица семейства врановых. Распространена в горах, на возвышенностях и морских побережьях Евразии и Северной Африки, на востоке ареала образует устойчивые городские поселения.',
+      image: './images/XC527413.jpg',
+      audio: './sounds/XC527413.mp3',
     },
     {
       id: 2,
-      name: 'Журавль',
-      species: 'Grus grus',
-      description: 'Звуки, издаваемые журавлем, похожи на звонкое «кур-лы – кур-лы». Журавли чаще всего поют дуэтом – одна птица начинает запев со слога «кур», а вторая подхватывает «лы». Если птица поёт одна, то она издает только звук «кур».',
-      image: 'https://live.staticflickr.com/65535/49221158846_b0b69a58f1.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/BLMSIUFTFU/XC512582-190604_1087_Grus_tok.mp3'
+      name: 'Зяблик',
+      species: 'Fringilla coelebs africana',
+      description: 'Зя́блик — певчая птица семейства вьюрковых. Численность в Западной Европе составляет 79—94 миллионов пар.',
+      image: './images/XC527406.jpg',
+      audio: './sounds/XC527406.mp3',
     },
     {
       id: 3,
-      name: 'Ласточка',
-      species: 'Delichon urbicum',
-      description: 'Для ласточек характерно негромкое щебетание. Песни ласточек не смолкают на протяжении всего лета. Исследователи различают у птиц до 6 щебечущих звуков: «вит», «ви-вит», «чивит», «чиривит» и т.п. Ласточки любят петь дуэтом.',
-      image: 'https://live.staticflickr.com//65535//48539007512_5029d2a9a0.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC489247-190724_09.10h_huiszwaluw_biesbosch_amaliahoeve_roep_100%2Bex_fouragerend_gezien_%20%282%29.mp3'
+      name: 'Московка',
+      species: 'Periparus ater',
+      description: 'Моско́вка, чёрная сини́ца, синица-московка или моховка — мелкая подвижная птица из семейства синицевых, широко распространённая в зоне лесов восточного полушария.',
+      image: './images/XC520704.jpg',
+      audio: './sounds/XC520704.mp3',
     },
     {
       id: 4,
-      name: 'Козодой',
-      species: 'Caprimulgus europaeus',
-      description: 'Козодой – неприметная птица, известная благодаря своему голосу. Песня козодоя звучит как монотонная трель похожая на тарахтение мотоцикла. Такое дребезжание слышно от заката до рассвета, его тональность, частота и громкость изменяются. ',
-      image: 'https://live.staticflickr.com/65535/48456345286_dbc8530027.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC486956-190623_22.37h_nachtzwaluw_rechte%20heide_zang_ad%20_2ex_gezien_.mp3'
+      name: 'Одноцветная нектарница',
+      species: 'Cinnyris batesi',
+      description: 'Вид распространён в Южной Африке. Встречается в Анголе, Демократической Республике Конго, Малави, Мозамбике, Руанде, Свазиленде, Уганде, Замбии и Южно-Африканской Республике. Обитает в финбоше, редколесье, садах и тому подобное.',
+      image: './images/XC508809.jpg',
+      audio: './sounds/XC508809.mp3',
     },
     {
       id: 5,
-      name: 'Кукушка',
-      species: 'Cuculus canorus',
-      description: 'Кукушку назвали так из-за особенностей ее песен. Звонкое «ку-ку» не спутать ни с какой другой птицей. Кукушки не строят гнезда, их потомство выращивают другие виды пернатых, которым кукушки подбрасывают свои яйца.',
-      image: 'https://live.staticflickr.com/65535/48377838151_e15f430ec1.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC501461-190616_08.13h_koekoek_brabantse%20biesbosch%20jantjesplaat_roep_1%20ex_ad%20m_ter%20plaatse%20zingend_gezien_.mp3'
+      name: 'Горихвостка-чернушка',
+      species: 'Phoenicurus ochruros',
+      description: 'Горихво́стка-черну́шка — мелкая певчая птица из семейства мухоловковых, распространённая в Европе и Центральной Азии. Несколько меньше воробья, выделяется прежде всего тёмным оперением большей части тела и ржавчато-оранжевым хвостом. Может зависать в воздухе, подобно колибри.',
+      image: './images/XC497730.jpg',
+      audio: './sounds/XC497730.mp3',
     },
     {
       id: 6,
-      name: 'Синица',
-      species: 'Parus major',
-      description: 'В щебетании синиц различают более 40 различных звуковых сочетаний. Поют они практически круглый год, немного затихая только зимой. Синицы настоящие санитары леса. Одна пара синиц в период гнездования оберегает от вредителей десятки деревьев.',
-      image: 'https://live.staticflickr.com//65535//49366042493_c48c81d58d.jpg',
-      audio: 'https://www.xeno-canto.org/sounds/uploaded/RFGQDPLDEC/XC518417-Kj%C3%B8ttmeis%20XC%20Helg%C3%B8ya%20Elias%20A.%20Ryberg20200108133922_079.mp3'
+      name: 'Каменный воробей',
+      species: 'Petronia petronia',
+      description: 'Обитает в предгорьях и на склонах гор, обычно в каменистых россыпях и на скалах. Полет каменного воробья ловкий и быстрый; по земле и камням он ходит и быстро бегает, как жаворонок.',
+      image: './images/XC467943.jpg',
+      audio: './sounds/XC467943.mp3',
     }
   ],
   [
